7a469e971108f3f1f415b9ac75c4ca282f4db587
shahanic <shahhhpark@gmail.com>
2024-06-08 12:41:05 +0800

    updated version

--- script.js
+++ script.js
@@ -1,48 +1,11 @@
-const evilButton = document.getElementById('evil-button')
-const OFFSET = 100
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector('.container');
 
-evilButton.addEventListener('click', () => {
-  alert('Nice Try')
-  window.close()
-})
+  container.addEventListener('mouseenter', () => {
+      container.style.transform = 'scale(1.05)';
+  });
 
-document.addEventListener('mousemove', (e) => {
-  const x = e.pageX
-  const y = e.pageY
-  const buttonBox = evilButton.getBoundingClientRect()
-  const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
-  const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
-  const horizontalOffset = buttonBox.width / 7 + OFFSET
-  const verticalOffset = buttonBox.height / 5 + OFFSET
-  if (Math.abs(horizontalDistanceFrom) <= horizontalOffset && Math.abs(verticalDistanceFrom) <= verticalOffset) {
-    setButtonPosition(
-      buttonBox.x + horizontalOffset / horizontalDistanceFrom *30,
-      buttonBox.y + verticalOffset / verticalDistanceFrom * 30
-    )
-  }
-})
-
-function setButtonPosition(left, top) {
-  const windowBox = document.body.getBoundingClientRect()
-  const buttonBox = evilButton.getBoundingClientRect()
-
-  if(distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
-    left = windowBox.right - buttonBox.width - OFFSET
-  }
-  if(distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
-    left = windowBox.left + OFFSET
-  }
-  if(distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
-    top = windowBox.bottom - buttonBox.height - OFFSET
-  }
-  if(distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
-    top = windowBox.top + OFFSET
-  }
-
-  evilButton.style.left = `${left}px`
-  evilButton.style.top = `${top}px`
-}
-
-function distanceFromCenter(boxPosition, mousePosition, boxSize) {
-  return boxPosition - mousePosition + boxSize / 2
-}
+  container.addEventListener('mouseleave', () => {
+      container.style.transform = 'scale(1)';
+  });
+});
